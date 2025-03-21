cc49b7234b26cbe9f574d17068ba5f6754bb672c
Deep <143270154+deepguchhait@users.noreply.github.com>
2025-03-21 07:25:35 +0530

    Add files via upload

---
+++ Profile/src/Data/Value.jsx
@@ -0,0 +1,15 @@
+
+const Data=[
+  {id:1,name:"deep",Roll:340,student: true,Markes:90,img:"../public/react.svg"},
+  {id:2,name:"sayan",Roll:340,student: true,Markes:90,img:"../public/vite.svg"},
+  {id:3,name:"sujit",Roll:340,student: true,Markes:90,img:"../public/react.svg"},
+  {id:4,name:"dev",Roll:340,student: true,Markes:90,img:"../public/vite.svg"},
+  {id:5,name:"nayan",Roll:340,student: true,Markes:90,img:"../public/react.svg"},
+  {id:6,name:"deep",Roll:340,student: true,Markes:90,img:"../public/vite.svg"},
+  {id:7,name:"deep",Roll:340,student: true,Markes:90,img:"../public/react.svg"},
+  {id:8,name:"deep",Roll:340,student: true,Markes:90,img:"../public/vite.svg"},
+  {id:9,name:"deep",Roll:340,student: true,Markes:90,img:"../public/react.svg"},
+  {id:10,name:"deep",Roll:340,student: true,Markes:59,img:"../public/vite.svg"}
+
+];
+export default Data;
